77108a900662a3a3d8b5d2cd9f43a21970422317
appgurung <laolex.stephen@gmail.com>
2018-08-06 14:23:55 +0100

    Project setup

---
+++ GuruLog/GuruLog.Core/Core.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GuruLog.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ GuruLog/GuruLog.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuruLog.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
